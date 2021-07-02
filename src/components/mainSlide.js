f630d629af2793a9a74a8e00081061f9c3401114
JP Baez <baezjuanpablo@gmail.com>
2021-07-02 17:29:30 +0200

    new main slide setup

--- src/components/mainSlide.js
+++ src/components/mainSlide.js
@@ -1,12 +1,80 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+//import Slider from "react-slick";
+//import "slick-carousel/slick/slick.css"; 
+//import "slick-carousel/slick/slick-theme.css";
 import "./mainSlide.css";
 
 
+function MainSlider ({list}) {
+    const [current, setCurrent] = useState(0);
+    const [featured, setFeatured] = useState([]);
 
+
+    useEffect(()=> {
+      function getSelection () {
+        const featList = list.filter(film => film.featured === true);
+        setFeatured(featList);
+        return
+      };  
+      getSelection();
+
+    },[]) 
+   
+    
+    
+
+    
+
+    const nextSlide = () => {
+      setCurrent( current === 3 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+      setCurrent( current === 0 ? 3 : current - 1);
+    }
+    
+    
+    
+ 
+  return (
+    <section className='main-slider'>
+      <button className="left-arrow" onClick={prevSlide}>prev</button>
+      <button className="right-arrow" onClick={nextSlide}>next</button>
+      {
+        featured.map((film, index)=> {
+            const slideImg = require("../images/" + film.Id + "frameA.jpg");
+              
+            return(
+              <div className={index=== current ? 'slide active' : 'slide'} index={index}>
+                
+                {index === current && (
+                  <>
+                  <Link to={`/filmpage/${film.slug}`}>
+                  <div className="slide-text">
+                  <h1>{film.title}</h1>
+                  <p>{film.synopsis}</p>
+                  </div>
+                  </Link>
+                  <img src={slideImg.default} alt="film frame" className="main-slider-pic" />
+                  </>
+                  )}
+                  
+     
+              </div>
+            ) 
+          }
+        
+          
+          
+        )
+      }
+    </section>
+  )
+}
+
+export default MainSlider;
+/* 
 export default class MainSlider extends Component {
 
   render() {
@@ -59,4 +127,4 @@ export default class MainSlider extends Component {
       </div>
     );
   }
-}
+} */
