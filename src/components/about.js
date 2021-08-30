93193b995bcefafab0d6e6428c944f5a34241467
JP Baez <baezjuanpablo@gmail.com>
2021-08-30 22:24:17 +0200

    ajustes css

--- src/components/about.js
+++ src/components/about.js
@@ -14,8 +14,8 @@ const About = () => {
                     <source src={About1} type='video/mp4' />
                 </video>
                 <div className="about-sub">
-                <h1>About</h1>
-                <p className='about-textA'>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. </p>
+                    <h1>About</h1>
+                    <p className='about-textA'>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. </p>
                 </div>
             </div>
             <div className="about-content">
--- src/components/nav.js
+++ src/components/nav.js
@@ -11,7 +11,12 @@ const NavBar = () => {
                 <Link to="/" className="logo-link"><img src={Logo} className="logo" alt="beatkino logo" /></Link>
             
             <div className="link-panel">
-                <Link to="/catalog"><img src={Search} className="search-icon" alt="search icon" /></Link>
+                <Link to="/catalog">
+                    <svg id="i-search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
+                    <circle cx="14" cy="14" r="12" />
+                    <path d="M23 23 L30 30"  />
+                    </svg>
+                </Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
             </div>
