f7e4d095ca1169cddacf3d3f0baad270628925e4
JP Baez <baezjuanpablo@gmail.com>
2022-01-21 13:55:23 +0100

    new pagination

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,41 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from 'react-redux'
-
 import Card from "./Card";
-import Pagination from './pagination';
+import ReactPaginate from "react-paginate";
+
 //import FilterButton from "./catalogButton";
 import './catalog.css';
 
 const Catalog = () => {  
     const list = useSelector((state) => state.list.list);
     const [filter, setFilter] = useState('');
-    const [catalogList, setCatalogList] = useState(list);
+    const [catalogList, setCatalogList] = useState([]);
     
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(10);
-
+    const [pageNumber, setPageNumber] = useState(0); 
+    
+    
+     // const [currentPage, setCurrentPage] = useState(1);
    
     const handleChange = (e) => {
         setFilter(e.target.value)
         };
-
-
     useEffect(()=>{
         function setupList () {
-            const filterInput = list.filter(film => film.title_eng.toLowerCase().includes(filter.toLowerCase()) || film.title_esp.toLowerCase().includes(filter.toLowerCase()) || film.director.toLowerCase().includes(filter.toLowerCase()) ||filter ==="" );
+            const listActive = list.filter(film => film.active=== true).reverse();
+            const filterInput = listActive.filter(film => film.title_eng.toLowerCase().includes(filter.toLowerCase()) || film.title_esp.toLowerCase().includes(filter.toLowerCase()) || film.director.toLowerCase().includes(filter.toLowerCase()) ||filter ==="" );
             setCatalogList(filterInput);
             return
         }
         setupList();    
     },[filter]) // eslint-disable-line react-hooks/exhaustive-deps
     
-     // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = catalogList.slice(indexOfFirstPost, indexOfLastPost);
 
-  // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+const filmsPerPage = 10;
+const pagesVisited = pageNumber * filmsPerPage;
+const displayFilms = catalogList
+                        .slice(pagesVisited, pagesVisited + filmsPerPage)
+                        .map((film, index) => {
+                            return <Card  film={film} key={index} />                           
+                        })
+const pageCount = Math.ceil(catalogList.length / filmsPerPage);
+const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+                           
+
+console.log(displayFilms)
 
     return(
         <div className="catalog-body">
@@ -43,25 +51,20 @@ const Catalog = () => {
                 <label htmlFor="searchInput">search:</label> 
                 <input type="text" name="searchInput" value={filter} onChange={handleChange} placeholder=" title or director" />
             </div>
-
             <div className="main-list">
-            {
-                currentPosts.map((film, index) => {
-                    if (film.active === true) {
-                        return( 
-                           
-                                <Card  film={film} key={index} />
-                                
-                        )
-                    } return null
-                }).reverse()                         
-            }
+                {displayFilms}
             </div>
-            <Pagination
-                postsPerPage={postsPerPage}
-                totalPosts={catalogList.length}
-                paginate={paginate}
-            />
+            <ReactPaginate 
+                previousLabel={"<"}
+                nextLabel={">"}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"paginationBttns"}
+                previousLinkClassName={"previousBttn"}
+                nextLinkClassName={"nextBttn"}
+                disabledClassName={"paginationDisabled"}
+                activeClassName={"paginationActive"}
+                />
         </div> 
     )
 }
