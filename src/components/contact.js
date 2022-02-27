c6fe1859a63dac3c0e51b454177c8c0862675dd7
JP Baez <baezjuanpablo@gmail.com>
2022-02-27 18:30:17 +0100

    contact css fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,14 +8,14 @@ const Contact = () => {
     const lang = useSelector((state) => state.lang.lang);
 
     return(
-    <>
+    <div className="contact-body">
         <h2 className="contact-title">{lang==="eng" ? "Contact":"Contacto"}</h2>
         <ContactForm />
         <div className="social-icon">
             <p>{lang==="eng" ?"also on":"también en"}</p>
             <a href="https://www.instagram.com/beatkino" target="_blank" rel="noreferrer" > <img className='icon' alt="instagram icon" src={InstaIcon}/> </a>             
         </div>
-    </>  
+    </div>  
     )
 }
 
