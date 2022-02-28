5815a6cb0a4eaa9972ee806a56b490329b9d27c7
JP Baez <baezjuanpablo@gmail.com>
2022-02-28 21:03:55 +0100

    translation fix

--- src/components/mainSlide.js
+++ src/components/mainSlide.js
@@ -9,7 +9,6 @@ import translate from "translate";
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 
-
 function MainSlider () {
     const lang = useSelector((state) => state.lang.lang)
     const list = useSelector((state) => state.list.list);
@@ -19,18 +18,17 @@ function MainSlider () {
    
     translate.engine = "deepl";
     translate.key = process.env.REACT_APP_DEEPL_KEY;
-    
-    useEffect(()=> {
-      function translationCountry () {
-        
-        feat.map( async item =>{
-          let translatedCountry = await translate(item.country, "es");
-          setCountryTrans(arr =>[ ...arr, [item.title_eng, translatedCountry]])
-        }) 
+
+    useEffect(()=>{
+      async function translateCountry () {
+          feat.map(async item =>{
+            let translatedCountry ={transCoun: await translate(item.country, "es")};
+            Object.assign(item,translatedCountry);
+            setCountryTrans((prev)=> [...prev, item])
+          })
       };
-      translationCountry ();
-     },[])  // eslint-disable-line react-hooks/exhaustive-deps
-  
+      translateCountry();
+    },[])
     
   return (
     <Swiper
@@ -52,34 +50,28 @@ function MainSlider () {
       modules={[Autoplay, Pagination, Navigation, EffectFade]}
       className="mySwiper"
       >
-      {feat.map((film, index) => {
+      {countryTrans.map((film, index) => {
         const filmFrame = require("../images/" + film.Id + "frameA.webp"); 
-        let countryTranslated = countryTrans.find(item => item[0]===film.title_eng)      
-        
           return (
-              <SwiperSlide key={index}>
-                <Link to={`/film/${film.slug}`}>
-                  <div className="slide-text">
-                    <div className="slide-header">
-                      <h2>{lang === "eng" ? film.title_eng : film.title_esp}</h2>
-                      <p className='slide-info'>{film.year} / {lang === "eng" ? film.country : countryTranslated[1] } / {film.runtime} min.</p>
-                    </div>
-                    <p className="slide-synopsis">{lang==="eng" ? film.log_line_eng: film.log_line_esp}</p>
+            <SwiperSlide key={index}>
+              <Link to={`/film/${film.slug}`}>
+                <div className="slide-text">
+                  <div className="slide-header">
+                    <h2>{lang === "eng" ? film.title_eng : film.title_esp}</h2>
+                    <p className='slide-info'>{film.year} / {lang === "eng" ? film.country : film.transCoun } / {film.runtime} min.</p>
                   </div>
-                </Link>
-                <img className='gallery-poster' 
-                  src={filmFrame.default}
-                  alt={film.title_eng}
-                  title={film.title_eng}
-                />
-              </SwiperSlide>
-            )
-          }   
-      )
-      }
-      
-      
-  </Swiper>
+                  <p className="slide-synopsis">{lang==="eng" ? film.log_line_eng: film.log_line_esp}</p>
+                </div>
+              </Link>
+              <img className='gallery-poster' 
+                src={filmFrame.default}
+                alt={film.title_eng}
+                title={film.title_eng}
+              />
+            </SwiperSlide>
+          )
+      })} 
+    </Swiper>
   
   )
 }
